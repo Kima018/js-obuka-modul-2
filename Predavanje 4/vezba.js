73bbdeb48649968ee3b03868bf9f0d2e82286924
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-10-31 15:02:17 +0100

    dodati comentari

--- Predavanje 4/vezba.js
+++ Predavanje 4/vezba.js
@@ -1,16 +1,17 @@
 var proizvodi = ["helb", "mleko", "jogurt"];
 
+// v01
 // var listaProizvoda = (document.getElementById("proizvodi").innerHTML =
 //   proizvodi);
-
+// v02
 // document.getElementById("proizvodi").innerHTML = proizvodi[0];
 // document.getElementById("proizvodi").innerHTML += " " + proizvodi[1];
 // document.getElementById("proizvodi").innerHTML += " " + proizvodi[2];
-
+// v03
 document.getElementById("proizvodi").innerHTML += `<p> ${proizvodi[0]}</p>`;
 document.getElementById("proizvodi").innerHTML += `<p> ${proizvodi[1]}</p>`;
 document.getElementById("proizvodi").innerHTML += `<p> ${proizvodi[2]}</p>`;
-
+// vezba
 document.getElementById(
   "proizvodi"
 ).innerHTML += `<p style='color:red'> ${proizvodi[0]}</p>`;
