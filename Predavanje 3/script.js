f1412a4dd0a98340481fb25827ac9f93119052ee
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-10-31 18:22:22 +0100

    ispisuje vreme u p tagu

--- Predavanje 3/script.js
+++ Predavanje 3/script.js
@@ -8,6 +8,9 @@ for (var i = 0; i < tekst.length; i++) {
   tekst[i].style.color = "green";
 }
 
+function dodajVreme() {
+  document.getElementById("vreme").innerHTML = Date();
+}
 /*var tekst = document.getElementsByClassName("tekst_sajta");
 tekst[0].style.color = "green";
 tekst[1].style.color = "pink";*/
