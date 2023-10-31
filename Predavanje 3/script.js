40b77b0c509388768c2d1ce18d945553098c0a45
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-10-31 13:38:50 +0100

    js dom

--- Predavanje 3/script.js
+++ Predavanje 3/script.js
@@ -1,1 +1,13 @@
-document.getElementById("naslovSajta").innerHTML = "Ovo je naslov iz JS";
+var naslov = document.getElementById("naslov_sajta");
+
+naslov.innerHTML = "Ovo je naslov iz Js";
+naslov.style.color = "red";
+
+var tekst = document.getElementsByClassName("tekst_sajta");
+for (var i = 0; i < tekst.length; i++) {
+  tekst[i].style.color = "green";
+}
+
+/*var tekst = document.getElementsByClassName("tekst_sajta");
+tekst[0].style.color = "green";
+tekst[1].style.color = "pink";*/
