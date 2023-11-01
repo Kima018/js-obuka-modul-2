3ab93fee870477049e4806a9b70dd169f0f5c2bc
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-11-01 17:16:33 +0100

    kreirana vezba 2

--- Predavanje 7/script.js
+++ Predavanje 7/script.js
@@ -1,1 +1,20 @@
-var godine = prompt("Koliko godina imate?");
+// var godine = prompt("Koliko godina imate?");
+// if (godine >= 18) {
+//   console.log("Punoletni ste, dobrodosli");
+// } else {
+//   console.log("Morate biti punoletni da vidite sadrzaj");
+// }
+
+var ime = prompt("Kako se zovete?");
+// console.log(typeof ime);
+console.log(ime);
+ime = ime.toLowerCase();
+console.log(ime);
+
+if (ime == "Maki") {
+  console.log(`Dobrodosao ${ime}!`);
+} else if (ime == "Petar") {
+  console.log(`Dobrodosao ${ime}!`);
+} else {
+  console.log("Pogresno ime, pristup je odbijen!");
+}
