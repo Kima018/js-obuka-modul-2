c5a683c0d322f58289d666c0aa9ee778ad9059bb
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-11-04 18:09:18 +0100

    odradjen domaci

--- Predavanje 12/vezba.js
+++ Predavanje 12/vezba.js
@@ -1,3 +1,71 @@
+var gradovi = [
+  {
+    ime: "Beograd",
+    temperatura: -2,
+    padavine: "sneg", //sneg, kisa, bez padavina
+  },
+  {
+    ime: "Subotica",
+    temperatura: 13,
+    padavine: "bez padavina",
+  },
+];
+// var gradovi = [];
+
+document.getElementById("dodajGrad").addEventListener("click", dodajGrad);
+document
+  .getElementById("topliGrad")
+  .addEventListener("click", prikaziTopliGrad);
+document
+  .getElementById("hladniGrad")
+  .addEventListener("click", prikaziHladniGrad);
+
+function dodajGrad() {
+  var porukaEl = document.getElementById("poruka");
+  var grad = prompt("unesi ime grada =>");
+  grad = grad.split(" ");
+  var privremeniGrad = {
+    ime: grad[0],
+    temperatura: grad[1],
+  };
+  gradovi.push(privremeniGrad);
+}
+
+function prikaziTopliGrad() {
+  var porukaEl = document.getElementById("poruka1");
+  porukaEl.innerHTML = "";
+  for (var grad in gradovi) {
+    grad = gradovi[grad];
+    if (grad["temperatura"] > 12) {
+      var paragraf = document.createElement("p");
+      paragraf.style.color = "red";
+      var paragrafText = document.createTextNode(
+        grad["ime"] + " " + `${grad["temperatura"]}°C`
+      );
+
+      paragraf.append(paragrafText);
+      document.getElementById("poruka1").append(paragraf);
+    }
+  }
+}
+
+function prikaziHladniGrad() {
+  var porukaEl = document.getElementById("poruka2");
+  porukaEl.innerHTML = "";
+  for (var grad in gradovi) {
+    grad = gradovi[grad];
+    if (grad["temperatura"] < 12) {
+      var paragraf = document.createElement("p");
+      paragraf.style.color = "blue";
+      var paragrafText = document.createTextNode(
+        grad["ime"] + " " + `${grad["temperatura"]}°C`
+      );
+      paragraf.append(paragrafText);
+      document.getElementById("poruka2").append(paragraf);
+    }
+  }
+}
+
 // var poruka = prompt("unesi ime grada =>");
 // var paragraf = document.createElement("p");
 // paragraf.append(poruka);
@@ -7,59 +75,16 @@
 // var grad = prompt("unesi ime grada =>");
 // document.getElementById("poruka").innerHTML = grad;
 
-var gradovi = [
-  // {
-  //   ime: "Beograd",
-  //   temperatura: -2,
-  //   padavine: "sneg", //sneg, kisa, bez padavina
-  // },
-  // {
-  //   ime: "Subotica",
-  //   temperatura: -4,
-  //   padavine: "bez padavina",
-  // },
-];
-
-document.getElementById("dodajGrad").addEventListener("click", dodajGrad);
-document.getElementById("topliGrad").addEventListener("click", prikaziGrad);
-document.getElementById("hladniGrad").addEventListener("click", prikaziGrad);
-
-var porukaEl = document.getElementById("poruka");
-
-function dodajGrad() {
-  var grad = prompt("unesi ime grada =>");
-  grad = grad.split(" ");
-
-  var privremeniGrad = {
-    ime: grad[0],
-    temperatura: grad[1],
-  };
-  gradovi.push(privremeniGrad);
-  console.log(gradovi);
-}
-
-function prikaziTopliGrad() {
-  porukaEl.innerHTML = "";
-  for (var grad in gradovi) {
-    grad = gradovi[grad];
-    var paragraf = document.createElement("p");
-    var paragrafText = document.createTextNode(
-      grad["ime"] + " " + grad["temperatura"]
-    );
-    paragraf.append(paragrafText);
-    document.getElementById("poruka").append(paragraf);
-  }
-}
-
-function prikaziHladniGrad() {
-  porukaEl.innerHTML = "";
-  for (var grad in gradovi) {
-    grad = gradovi[grad];
-    var paragraf = document.createElement("p");
-    var paragrafText = document.createTextNode(
-      grad["ime"] + " " + grad["temperatura"]
-    );
-    paragraf.append(paragrafText);
-    document.getElementById("poruka").append(paragraf);
-  }
-}
+// function prikaziHladniGrad() {
+// var porukaEl = document.getElementById("poruka");
+//   porukaEl.innerHTML = "";
+//   for (var grad in gradovi) {
+//     grad = gradovi[grad];
+//     var paragraf = document.createElement("p");
+//     var paragrafText = document.createTextNode(
+//       grad["ime"] + " " + grad["temperatura"]
+//     );
+//     paragraf.append(paragrafText);
+//     document.getElementById("poruka").append(paragraf);
+//   }
+// }
