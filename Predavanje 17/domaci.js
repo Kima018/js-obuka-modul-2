1a119697f66b0a96e812762b7faa6521ccc941dd
Kima018 <jakovljevic.marijan.nis@gmail.com>
2023-11-23 13:00:39 +0100

    domaci 17 izmenjen

--- Predavanje 17/domaci.js
+++ Predavanje 17/domaci.js
@@ -1,5 +1,25 @@
 const key = "aace614e5c374ad194f145139231311";
 const url = "http://api.weatherapi.com/v1/forecast.json";
+function updateWeather(city) {
+  $.ajax({
+    method: "GET",
+    url: url,
+    data: {
+      key: key,
+      q: city,
+      days: 1,
+      aqi: "no",
+      alerts: "no",
+    },
+    success: function (response) {
+      var temp = response["current"]["temp_c"];
+      var cityName = response["location"]["name"];
+      $("#selectedInfo").text(
+        "Grad: " + cityName + ", temperatura: " + temp + "°C"
+      );
+    },
+  });
+}
 $(document).ready(function () {
   var lastSelected = localStorage.getItem("select");
   if (lastSelected !== null) {
@@ -8,27 +28,6 @@ $(document).ready(function () {
 
   var selectedCity = $("#selectCity").val();
   localStorage.setItem("select", selectedCity);
-
-  function updateWeather(city) {
-    $.ajax({
-      method: "GET",
-      url: url,
-      data: {
-        key: key,
-        q: city,
-        days: 1,
-        aqi: "no",
-        alerts: "no",
-      },
-      success: function (response) {
-        var temp = response["current"]["temp_c"];
-        var cityName = response["location"]["name"];
-        $("#selectedInfo").text(
-          "Grad: " + cityName + ", temperatura: " + temp + "°C"
-        );
-      },
-    });
-  }
 
   updateWeather(selectedCity);
 
